758bec948c9b3af09ebca9e76076b99a4898c778
Coops0 <51346812+Coops0@users.noreply.github.com>
2024-12-11 12:45:37 -0500

    d

--- build.rs
+++ build.rs
@@ -2,10 +2,12 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=templates/");
+    println!("cargo:rerun-if-changed=assets/user-script.js");
     let status = Command::new("./tailwindcss")
         .args(["-i", "assets/index.css", "-o", "assets/ts.css", "-m"])
         .status()
         .unwrap();
 
+    println!("cargo::warning=tailwind build status: {status:?}");
     assert!(status.success());
 }
