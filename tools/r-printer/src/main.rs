b64725f0250330d7827f83714e26c002e1388035
Coops0 <51346812+Coops0@users.noreply.github.com>
2024-12-15 15:47:37 -0500

    use clap parser instead of inquirer

--- tools/r-printer/src/main.rs
+++ tools/r-printer/src/main.rs
@@ -1,21 +1,39 @@
 use anyhow::Context;
+use clap::Parser;
 use escpos::driver::NativeUsbDriver;
 use escpos::printer::Printer;
 use escpos::utils::{JustifyMode, Protocol, QRCodeCorrectionLevel, QRCodeModel, QRCodeOption};
-use inquire::validator::Validation;
-use inquire::{Confirm, CustomType, Text};
 
 const MANUFACTURER: &str = "EPSON";
 const PRODUCT_NAME: &str = "TM-T88V";
 
 const URI_PREFIX: &str = "https://studyport.net/l/";
 
-fn main() -> anyhow::Result<()> {
-    let verbose = std::env::args().any(|a| &a == "-v" || &a == "--verbose");
+#[derive(Parser)]
+struct Args {
+    /// Print all usb devices
+    #[clap(short, long, default_value_t = false)]
+    debug: bool,
 
-    let (vendor_id, product_id) = nusb::list_devices()?
+    /// The code of the location (will be appended to the URI_PREFIX)
+    #[clap(short, long, required = true, value_parser = lowercase_non_empty_string_parser)]
+    name: String,
+
+    /// The text to print below the QR code
+    #[clap(short, long, required = false)]
+    subtext: Option<String>,
+
+    /// The amount of copies to print
+    #[clap(short, long, required = false, default_value_t = 1, value_parser = non_zero_i8_parser)]
+    amount: i8,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let printer_usb_device = nusb::list_devices()?
         .inspect(|d| {
-            if verbose {
+            if args.debug {
                 println!("{:?}", d);
             }
         })
@@ -23,55 +41,50 @@ fn main() -> anyhow::Result<()> {
             d.manufacturer_string().unwrap_or_default() == MANUFACTURER
                 && d.product_string().unwrap_or_default() == PRODUCT_NAME
         })
-        .map(|d| (d.vendor_id(), d.product_id()))
         .context("printer not found")?;
-
-    let name = Text::new("location code?")
-        .with_formatter(&str::to_lowercase)
-        .with_validator(|n: &str| {
-            Ok(if !n.is_empty() {
-                Validation::Valid
-            } else {
-                Validation::Invalid("not enough".into())
-            })
-        })
-        .prompt()?;
-
-    let subtext = Text::new("subtext?").with_default(&name).prompt().unwrap_or_default();
-
-    let amount = CustomType::<i8>::new("how many to print?")
-        .with_default(1)
-        .with_validator(|&a: &i8| {
-            Ok(if a > 0 { Validation::Valid } else { Validation::Invalid("not enough".into()) })
-        })
-        .prompt()?;
+    
+    println!("printer found with vendor id: {:#06x}, product id: {:#06x}", printer_usb_device.vendor_id(), printer_usb_device.product_id());
 
     let mut printer =
-        Printer::new(NativeUsbDriver::open(vendor_id, product_id)?, Protocol::default(), None);
-
-    if !Confirm::new("print?").prompt()? {
-        return Ok(());
-    }
+        Printer::new(NativeUsbDriver::open(printer_usb_device.vendor_id(), printer_usb_device.product_id())?, Protocol::default(), None);
 
     printer.init()?;
 
-    let qr_code = format!("{URI_PREFIX}{name}");
+    let qr_code = format!("{URI_PREFIX}{}", args.name);
     println!("qr code link: {qr_code}");
 
-    for i in 0..amount {
-        println!("{}/{amount}", i + 1);
+    for i in 0..args.amount {
+        println!("{}/{}", i + 1, args.amount);
 
         printer.justify(JustifyMode::CENTER)?.qrcode_option(
             &qr_code,
             QRCodeOption::new(QRCodeModel::Model2, 16, QRCodeCorrectionLevel::H),
         )?;
 
-        if !subtext.is_empty() {
-            printer.feed()?.smoothing(true)?.bold(true)?.size(2, 2)?.write(&subtext)?;
+        if let Some(subtext) = &args.subtext {
+            printer.feed()?.smoothing(true)?.bold(true)?.size(2, 2)?.write(subtext)?;
         }
 
         printer.feeds(3)?.print_cut()?;
     }
 
     Ok(())
+}
+
+fn lowercase_non_empty_string_parser(s: &str) -> Result<String, String> {
+    let s = s.trim().to_lowercase();
+    if s.is_empty() {
+        Err("empty string".to_string())
+    } else {
+        Ok(s)
+    }
+}
+
+fn non_zero_i8_parser(s: &str) -> Result<i8, String> {
+    let n = s.parse::<i8>();
+
+    match n {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err("not a positive integer".to_string()),
+    }
 }
